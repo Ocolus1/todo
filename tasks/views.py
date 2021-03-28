ea3270e1144b3a5bb9fefb6dc09dd6cdd4541333
Cypherspot <awolesiboluwatife@gmail.com>
2021-03-28 04:00:41 +0100

    Made it a available for user session

--- tasks/views.py
+++ tasks/views.py
@@ -1,42 +1,77 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
-import os
+import string
+from random import choices
+
+def generate_id():
+    characters = string.digits + string.ascii_letters
+    short_url = "".join(choices(characters, k=6))
+
+    return short_url
 
 # Create your views here.
 def index(request):
-    tasks = Task.objects.all().order_by("-id")
-    form = TaskForm()
-
+    if "tasks" not in request.session:
+        request.session["tasks"] = [] 
+    
     if request.method == "POST":
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
+        id = generate_id()
+        title = request.POST['title']
+        completed = request.POST.get('completed', None)
+        saved_list = request.session['tasks']
+        if completed:
+            saved_list.append({"id": id, "title": title, "completed": True})
+        else:
+            saved_list.append({"id": id, "title": title, "completed": False})
+        
+        request.session['tasks'] = saved_list
+
         return redirect("/")
-    context = {"tasks":  tasks , "form": form}
+
+    tasks = request.session["tasks"]
+    context = {"tasks":  tasks}
     return render(request, 'tasks/index.html', context)
 
 
 def update(request, id):
-    task = Task.objects.get(pk=id)
-
-    form = TaskForm(instance=task)
+    saved_list = request.session['tasks']
+    for i in saved_list:
+        if i['id'] == id:
+            task = i
 
     if request.method == "POST":
-        form = TaskForm(request.POST or None, instance=task)
-        if form.is_valid():
-            form.save()
-            return redirect("/")
+        new_title = request.POST['title']
+        completed = request.POST.get('completed', None)
+        saved_list = request.session['tasks']
+        for i in saved_list:
+            if i['id'] == id:
+                i['title'] = new_title
+                if completed:
+                    i['completed'] = True
+                else:
+                    i['completed'] = False
+            break
+        request.session['tasks'] = saved_list
+        return redirect("/")
 
-    context = {"form": form}
+    context = {"task": task}
     return render(request, "tasks/update.html", context)
 
 
 def delete_task(request, id):
-    task = Task.objects.get(pk=id)
+    saved_list = request.session['tasks']
+    for i in saved_list:
+        if i['id'] == id:
+            task = i
 
     if request.method == "POST":
-        task.delete()
+        saved_list = request.session['tasks']
+        for i in saved_list:
+            if i['id'] == id:
+                saved_list.remove(i)
+
+        request.session['tasks'] = saved_list
         return redirect("/")
 
 
